1888da54c90e90ae9a0ef1ffb0ef287e803e747f
st945306 <st945306@gmail.com>
2015-12-02 22:08:36 +0800

    remove some dots in mux8.v

--- mux8.v
+++ mux8.v
@@ -1,11 +1,11 @@
 module mux8(
-	.HD_i		
-	.Control_i		
-	.data_o
+	HD_i		
+	Control_i		
+	data_o
 );
 
 input 				HD_i;
-input	[7:0]		.Control_i;
+input	[7:0]		Control_i;
 output	reg [7:0]	data_o;
 
 always@(*)begin
