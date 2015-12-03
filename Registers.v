0c2222cd4a15d53422c4caf866d25b835bf1d4ba
DearLinny <b02902045@ntu.edu.tw>
2015-12-03 16:37:26 +0800

    rename

--- Registers.v
+++ Registers.v
@@ -2,36 +2,33 @@
 module Registers
 (
     clk_i,
-    RSaddr_i,
-    RTaddr_i,
-    RDaddr_i, 
-    RDdata_i,
+    ReadRegister1_i,
+    ReadRegister2_i,
+    WriteRegister_i, 
+    WriteData_i,
     RegWrite_i, 
-    RSdata_o, 
-    RTdata_o 
+    ReadData1_o, 
+    ReadData2_o 
 );
 
 // Ports
-input               clk_i;
-input   [4:0]       RSaddr_i;
-input   [4:0]       RTaddr_i;
-input   [4:0]       RDaddr_i;
-input   [31:0]      RDdata_i;
-input               RegWrite_i;
-output  [31:0]      RSdata_o; 
-output  [31:0]      RTdata_o;
+input               clk_i, RegWrite_i;
+input   [4:0]       ReadRegister1_i, ReadRegister2_i, WriteRegister_i;
+input   [31:0]      WriteData_i;
+
+output  [31:0]      ReadData1_o, ReadData2_o; 
 
 // Register File
 reg     [31:0]      register        [0:31];
 
 // Read Data      
-assign  RSdata_o = register[RSaddr_i];
-assign  RTdata_o = register[RTaddr_i];
+assign  ReadData1_o = register[ReadRegister1_i];
+assign  ReadData2_o = register[ReadRegister2_i];
 
 // Write Data   
 always@(posedge clk_i) begin
     if(RegWrite_i)
-        register[RDaddr_i] <= RDdata_i;
+        register[WriteRegister_i] <= WriteData_i;
 end
    
 endmodule 
