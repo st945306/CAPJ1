079667fd5d383ca71d76b49395043c1d29b23aeb
DearLinny <b02902045@ntu.edu.tw>
2015-12-03 21:49:08 +0800

    fix condition

--- Data_memory.v
+++ Data_memory.v
@@ -13,7 +13,7 @@ module Data_memory(
 	reg [7:0] memory [0:31];
 
 
-always@(clk_i or Address_i or WriteData_i or MemWrite_i or MemRead_i)begin
+always@(posedge clk_i or Address_i or WriteData_i or MemWrite_i or MemRead_i)begin
 	if(MemRead_i)begin
 		data_o = {
 		    memory[Address_i + 3],
@@ -22,7 +22,7 @@ always@(clk_i or Address_i or WriteData_i or MemWrite_i or MemRead_i)begin
         	memory[Address_i]
     	};
 	end
-	else if(MemWrite_i)begin
+	else if(posedge clk_i && MemWrite_i)begin
 		memory[Address_i + 3] <= WriteData_i[31:24];
 		memory[Address_i + 2] <= WriteData_i[23:16];
 		memory[Address_i + 1] <= WriteData_i[15:8];
